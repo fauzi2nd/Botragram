6485c8fb975e0399b818bb991a669b78d8832895
Fauzi Pranata <spacetone2nd@gmail.com>
2026-07-20 07:09:40 +0700

    Update enums.py

--- models/enums.py
+++ models/enums.py
@@ -29,7 +29,7 @@ __all__ = [
     "RiskLevel",
     "TrendDirection",
     "TrendStrength",
-    "MarketState",
+    "MarketCondition",
     "VolatilityLevel",
     "VolumeStrength",
     "PriceSource",
